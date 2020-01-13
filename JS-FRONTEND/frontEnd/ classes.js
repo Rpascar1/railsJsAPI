c56f28eb8e5e1330373cbcef7bc51aa97d635607
Rich <rpavesi84@gmail.com>
2020-01-13 18:12:02 -0500

    added test images for draw cards and front end

---
+++ JS-FRONTEND/frontEnd/ classes.js
@@ -0,0 +1,17 @@
+class Game {
+
+  this.scores = scores
+  this.roundScore = null
+  this.dice1 =  null
+  this.dice2 = null
+  this.lastDice = null
+  this.gamePlaying = null
+  //this.activePlayer = activePlayer
+  this.winningScore = winningScore
+}
+
+
+class Player {
+
+ this.activePlayer = activePlayer
+}
