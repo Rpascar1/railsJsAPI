e53b8edd7e8ec9d33d702fa0cb8e360928dc5cff
Rich <rpavesi84@gmail.com>
2020-03-10 14:47:08 -0400

    Prepared procfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-      namespace :api do
-        namespace :v1 do
-          resources :players
-          resources :games
-
-            end
-          end
-        end
+  root to: 'home#index'
+  namespace :api do
+    namespace :v1 do
+      resources :players
+      resources :games
+    end
+  end
+end
